e6114090d97c598f196d7f1bc158ec9bf2b0ab70
DilumN <dilumnavanjana@gmail.com>
2016-06-19 15:54:52 +0530

    added blank & time executions

--- lib/gitstats/cli.rb
+++ lib/gitstats/cli.rb
@@ -3,7 +3,24 @@ module Gitstats
   class CLI
 
   	def initialize(*)
-  		p ARGV
+  		if ARGV.empty?
+  			blank
+  		elsif ARGV[0] == "time"
+  			time  					
+  		end
+  			
+  	end
+
+  	def blank
+  		str = %q{git log --format='%aN'  | sort -u | while read name; do echo "Author >>>> " "$name\t"; git log --numstat --pretty="%H" --author="$name" | awk 'NF==3 {plus+=$1; minus+=$2} NF==1 {total++} END {printf("******************************************\nlines added: \t +%d lines \ndeleted: \t -%d lines \ntotal commits: \t %d\n******************************************\n\n", plus, minus, total)}' -; done}
+  		exec str
+  	end
+
+  	def time(*)
+  		from = ARGV[1]
+  		to = ARGV[2]
+			str = %q{git log --format='%aN'  | sort -u | while read name; do echo "Author >>>> " "$name\t"; git log --numstat --pretty="%H" --author="$name"} + %{ --since '#{from}' --until '#{to}' } + %q{| awk 'NF==3 {plus+=$1; minus+=$2} NF==1 {total++} END {printf("******************************************\nlines added: \t +%d lines \ndeleted: \t -%d lines \ntotal commits: \t %d\n******************************************\n\n", plus, minus, total)}' -; done}
+			exec str
   	end
 
   end
