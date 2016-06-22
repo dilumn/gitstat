bdd1d3d136fbcf9a26ec994dc959bb33e66c083a
DilumN <dilumnavanjana@gmail.com>
2016-06-22 12:23:01 +0530

    added spaces to all

--- lib/gitstat/cli.rb
+++ lib/gitstat/cli.rb
@@ -5,13 +5,13 @@ module Gitstat
       if ARGV.empty?
         blank
       elsif ARGV[0] == "time"
-        time            
+        time
       end
     end
 
     def blank
       str = %q{git log --format='%aN'  | sort -u | while read name; do echo "Author >>>> " "$name"; git log --numstat --pretty="%H" --author="$name" | awk 'NF==3 {plus+=$1; minus+=$2} NF==1 {total++} END {printf("lines added: +%d lines \t | \t deleted: -%d lines \t | \t total commits: %d\n****************************************\n\n", plus, minus, total)}' -; done}
-      
+
       system "bash", "-c", str
     end
 
@@ -19,7 +19,7 @@ module Gitstat
       from = ARGV[1]
       to = ARGV[2]
       str = %q{git log --format='%aN'  | sort -u | while read name; do echo "Author >>>> " "$name"; git log --numstat --pretty="%H" --author="$name"} + %{ --since '#{from}' --until '#{to}' } + %q{| awk 'NF==3 {plus+=$1; minus+=$2} NF==1 {total++} END {printf("******************************************\nlines added: \t +%d lines \ndeleted: \t -%d lines \ntotal commits: \t %d\n******************************************\n\n", plus, minus, total)}' -; done}
-      
+
       system "bash", "-c", str
     end
   end
